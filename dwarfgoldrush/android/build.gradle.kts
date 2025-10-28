2a3512883147623260fc5c565aa52b328d93b1e3
ManoelMessias32 <manoeletrico2hotmail.com@gmail.com>
2025-10-28 18:11:15 -0300

    Atualiza dwarfgoldrush para android

--- dwarfgoldrush/android/build.gradle.kts
+++ dwarfgoldrush/android/build.gradle.kts
@@ -1,5 +1,9 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    id("com.android.application") version "8.13.0" apply false
+// ARQUIVO: C:/Games/dwarfgoldrush/android/build.gradle.kts (Projeto Raiz)
+
+plugins {    // Apenas DECLARA os plugins que os módulos podem usar. O "apply false" é crucial.
+    id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 }
+
+
+
