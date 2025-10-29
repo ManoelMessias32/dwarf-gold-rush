22b57cb4702be1070fdd1bc8af27cd53a10f8e95
ManoelMessias32 <manoeletrico2hotmail.com@gmail.com>
2025-10-28 22:54:55 -0300

    Adiciona ícones e regras de backup padrão.

--- dwarfgoldrush/android/build.gradle.kts
+++ dwarfgoldrush/android/build.gradle.kts
@@ -1,9 +1,5 @@
-// ARQUIVO: C:/Games/dwarfgoldrush/android/build.gradle.kts (Projeto Raiz)
-
-plugins {    // Apenas DECLARA os plugins que os módulos podem usar. O "apply false" é crucial.
-    id("com.android.application") version "8.2.0" apply false
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "8.4.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 }
-
-
-
